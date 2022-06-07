2cc567ac59a1098bd0f8fe81460b64428f1177fc
bigrainlin <73051875+bigrainlin@users.noreply.github.com>
2022-06-06 20:10:09 -0500

    Revert "update from bigrain"
    This reverts commit c1b5c841c4fb55befcd9952df97d9dff113b9528.

--- target/compiled/taxi_rides_ny/models/staging/schema.yml/not_null_stg_green_tripdata_tripid.sql
+++
@@ -1,9 +0,0 @@
-
-    
-    
-
-select *
-from `de-bootcamp-350619`.`trips_data_all`.`stg_green_tripdata`
-where tripid is null
-
-
--- target/compiled/taxi_rides_ny/models/staging/schema.yml/not_null_stg_yellow_tripdata_tripid.sql
+++
@@ -1,9 +0,0 @@
-
-    
-    
-
-select *
-from `de-bootcamp-350619`.`trips_data_all`.`stg_yellow_tripdata`
-where tripid is null
-
-
